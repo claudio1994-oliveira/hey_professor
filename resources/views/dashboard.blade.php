58e4765b7282c4d729ed341bf44e8f650d6ed26f
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-06-02 15:14:02 -0300

    hey-11: being able to see the list of questions in the dashborard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,5 +18,14 @@
                 Cancel
             </x-btn.info>
         </x-form>
+
+        <hr class="border-gray-700 border-dashed my-4">
+        <div class="dark:text-gray-600 uppercase font-bold mb-1"> Question List</div>
+        <div class="dark:text-gray-400 space-y-4">
+            @foreach ($questions as $q)
+                <x-question :item="$q"></x-question>
+            @endforeach
+        </div>
+
     </x-container>
 </x-app-layout>
