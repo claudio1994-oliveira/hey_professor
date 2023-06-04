5c878c3a6a7e925639f687d315b627792ead790e
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-06-04 15:28:06 -0300

    hey-11: be able like a question

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Question\LikeController;
 use App\Http\Controllers\{DashboardController, ProfileController, QuestionController};
 use Illuminate\Support\Facades\Route;
 
@@ -27,6 +28,8 @@ Route::get('/', function () {
 Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
+Route::post('/question/like/{question}', LikeController::class)->name('question.like');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
