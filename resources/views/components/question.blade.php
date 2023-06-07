c6e69e6381eda6b97a43d8dd5031b38b842b6631
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-06-06 21:24:48 -0300

    hey-11: Handling n + 1 issue

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -8,14 +8,14 @@
         <x-form :action="route('question.like', $item)">
             <button type="submit" class="flex items-start space-x-1 text-green-500 ">
                 <x-icons.thumbs-up class="w-5 h-5 hover:text-green-300 cursor-pointer" />
-                <span> {{ $item->likes }} </span>
+                <span> {{ $item->votes_sum_like ?: 0 }} </span>
             </button>
         </x-form>
         <x-form :action="route('question.unlike', $item)">
             <button type="submit" href="{{ route('question.like', $item) }}"
                 class="flex items-start space-x-1 text-red-500 ">
                 <x-icons.thumbs-down class="w-5 h-5  hover:text-red-300 cursor-pointer" />
-                <span> {{ $item->unlikes }} </span>
+                <span> {{ $item->votes_sum_unlike ?: 0 }} </span>
             </button>
         </x-form>
     </div>
