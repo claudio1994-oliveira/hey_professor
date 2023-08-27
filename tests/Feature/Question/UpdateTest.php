8f322bbb900c8ea962b965f18864abf93872ba02
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-08-27 15:30:30 -0300

    hey-16: make sure that is validating everthing when update a question

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -2,7 +2,7 @@
 
 use App\Models\{Question, User};
 
-use function Pest\Laravel\{actingAs, put};
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, put};
 
 it('should update the question in the database', function () {
     $user = User::factory()->create();
@@ -43,4 +43,34 @@ it('should make sure that only the person who has created the question can updat
     actingAs($rightUser);
 
     put(route('question.update', $question))->assertRedirect();
+});
+
+it('shoul be npt able to update a question bigger than 255 characters', function () {
+
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $question = Question::factory()->create(['draft' => true, 'created_by' => $user->id]);
+
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 260) . '?',
+    ]);
+
+    $request->assertRedirect();
+    assertDatabaseCount('questions', 1);
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 260) . '?']);
+});
+
+it('should check if with question mark ?', function () {
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $question = Question::factory()->create(['draft' => true, 'created_by' => $user->id]);
+
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 10),
+    ]);
+
+    $request->assertSessionHasErrors(['question' => 'Are you sure tha is a question? It is missing te question mark in the end.']);
+    assertDatabaseHas('questions', ['question' => $question->question]);
 });
