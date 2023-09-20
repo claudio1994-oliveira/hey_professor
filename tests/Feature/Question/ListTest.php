27334f18b01a538f8402d9526dd20e57e1c175a5
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2023-09-19 22:02:56 -0300

    hey-18: should order by like and unlike, most liked question should be at the top, most unliked questions should be in the bottom

--- tests/Feature/Question/ListTest.php
+++ tests/Feature/Question/ListTest.php
@@ -31,3 +31,29 @@ it('should paginate the result', function () {
             return $value instanceof LengthAwarePaginator;
         });
 });
+
+it('should order by like and unlike, most liked question should be at the top, most unliked questions should be in the bottom', function () {
+    $user       = User::factory()->create();
+    $secondUser = User::factory()->create();
+
+    Question::factory()->count(5)->create();
+
+    actingAs($user);
+
+    $mostLikedQuestion   = Question::find(3);
+    $mostUnlikedQuestion = Question::find(1);
+
+    $user->like($mostLikedQuestion);
+    $secondUser->unlike($mostUnlikedQuestion);
+
+    get(route('dashboard'))
+        ->assertViewHas('questions', function ($questions) {
+
+            expect($questions)
+                ->first()->id->toBe(3)
+                ->and($questions)
+                ->last()->id->toBe(1);
+
+            return true;
+        });
+});
